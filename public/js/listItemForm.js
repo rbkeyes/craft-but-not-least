0876c11deaa4953b03e200b16eaaa724d7141c07
Reed Keyes <rbkeyes@gmail.com>
2023-02-08 10:10:16 -0800

    list item form handler updated. Won't be sure of functionality once the site is up and running but basics should be there

--- public/js/listItemForm.js
+++ public/js/listItemForm.js
@@ -1,33 +1,39 @@
+// ⚛︎✧⚙︎※☸︎❂✛⚙︎☄︎✢☺︎▵♥︎➣☼ Reed ⚛︎✧⚙︎※☸︎❂✛⚙︎☄︎✢☺︎▵♥︎➣☼
 const listItemFormHandler = async (event) => {
-    // Stop the browser from submitting the form so we can do so with JavaScript
-    event.preventDefault();
-  
-    // Gather the data from the form elements on the page
-    const itemName = document.getElementById('item-name').value.trim();
-    const description = document.getElementById('description').value.trim();
-    const listPrice = document.getElementById('list-price').value.trim();
-    // not 100% sure what we'll need for for the photo upload, adding this for now but can edit later as needed
-    const photoUpload = document.getElementById('photo-upload');
-    // there will be multiple tags, want to get all of them
-    const tag = document.querySelectorAll('#tag');
-    
-    if (email && password) {
-      // Send the e-mail and password to the server
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to log in');
-      }
+  // Stop the browser from submitting the form so we can do so with JavaScript
+  event.preventDefault();
+
+  // Gather the data from the form elements on the page
+  const itemName = document.getElementById('item-name').value.trim();
+  const description = document.getElementById('description').value.trim();
+  const listPrice = document.getElementById('list-price').value.trim();
+  // ⚛︎✧⚙︎※☸︎❂✛⚙︎☄︎✢☺︎▵♥︎➣☼ 
+  // not 100% sure what we'll need for for the photo upload, adding this for now but can edit later as needed 
+  // ⚛︎✧⚙︎※☸︎❂✛⚙︎☄︎✢☺︎▵♥︎➣☼
+  const photoUpload = document.getElementById('photo-upload');
+  // ⚛︎✧⚙︎※☸︎❂✛⚙︎☄︎✢☺︎▵♥︎➣☼ 
+  // there will be multiple tags, want to get all of them
+  // ⚛︎✧⚙︎※☸︎❂✛⚙︎☄︎✢☺︎▵♥︎➣☼
+  const tag = document.querySelectorAll('#tag');
+
+  if (itemName && description && listPrice && tag) { 
+    // ⚛︎✧⚙︎※☸︎❂✛⚙︎☄︎✢☺︎▵♥︎➣☼add photoUpload when ready
+    // Send the item data to the server
+    const response = await fetch('/api/users/listItem', {
+      method: 'POST',
+      body: JSON.stringify({ itemName, description, listPrice, tag }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+      // ⚛︎✧⚙︎※☸︎❂✛⚙︎☄︎✢☺︎▵♥︎➣☼ route to ?? page after POST
+    } else {
+      alert('Unable to list item.');
     }
-  };
-  
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
-  
+  }
+};
+
+document
+  .getElementById('.list-item-form')
+  .addEventListener('submit', listItemFormHandler);
