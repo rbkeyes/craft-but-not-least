fc83670bde87a4effea3140b73dc9b655a4dc4b5
Reed Keyes <rbkeyes@gmail.com>
2023-02-07 21:52:57 -0800

    listItem handlebars in progress

---
+++ public/js/listItem.js
@@ -0,0 +1,29 @@
+const listItemFormHandler = async (event) => {
+    // Stop the browser from submitting the form so we can do so with JavaScript
+    event.preventDefault();
+  
+    // Gather the data from the form elements on the page
+    const itemName = document.querySelector('#item-name').value.trim();
+    const description = document.querySelector('#description').value.trim();
+    
+  
+    if (email && password) {
+      // Send the e-mail and password to the server
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log in');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+  
