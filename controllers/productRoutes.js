1342d2a2ec9b862a1ddf01e1d2c1824f420b1bc6
mengxue xu <mxu4321@gmail.com>
2023-02-09 14:50:33 -0800

    updated ./controllers/sellProductRoutes

--- controllers/productRoutes.js
+++ controllers/productRoutes.js
@@ -1,1 +1,92 @@
+// ☆•:*´¨`*:•.☆•:*´¨`*:•.Mengxue☆•:*´¨`*:•.☆•:*´¨`*:•.☆•:*´¨
 // for user to buy products and contact seller
+const router = require('express').Router();
+const { Product, User, Tag } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// get all products
+router.get('/', async (req, res) => {
+    try {
+        const productData = await Product.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+                {
+                    model: Tag,
+                    attributes: ['tag_name'],
+                },
+            ],
+        });
+        const products = productData.map((product) => product.get({ plain: true }));
+        res.render('all-products', { products, logged_in: req.session.logged_in });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// get one product by id
+router.get('/:id', async (req, res) => {
+    try {
+        const productData = await Product.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+                {
+                    model: Tag,
+                    attributes: ['tag_name'],
+                },
+            ],
+        });
+        const product = productData.get({ plain: true });
+        res.render('product', { product, logged_in: req.session.logged_in });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// get all products by tag id
+router.get('/tag/:id', async (req, res) => {
+    try {
+        const productData = await Product.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+                {
+                    model: Tag,
+                    attributes: ['tag_name'],
+                    where: {
+                        id: req.params.id,
+                    },
+                },
+            ],
+        });
+        const products = productData.map((product) => product.get({ plain: true }));
+        res.render('all-products', { products, logged_in: req.session.logged_in });
+    } catch (err) { 
+        res.status(500).json(err);
+    }
+});
+
+// ==========⚠️NOT SURE if working=========== contact seller
+router.post('/contact/:id', withAuth, async (req, res) => {
+    try {
+        const newContact = await Contact.create({
+            ...req.body,
+            user_id: req.session.user_id,
+            product_id: req.params.id,
+        });
+        res.status(200).json(newContact);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
+module.exports = router;
+// // ☆•:*´¨`*:•.☆•:*´¨`*:•.Mengxue☆•:*´¨`*:•.☆•:*´¨`*:•.☆•:*´¨
--- controllers/sellProductRoutes.js
+++ controllers/sellProductRoutes.js
@@ -1,1 +1,86 @@
+// ☆•:*´¨`*:•.☆•:*´¨`*:•.Mengxue☆•:*´¨`*:•.☆•:*´¨`*:•.☆•:*´¨
 // for user to listing and sell products
+const router = require("express").Router();
+const { Product, User, Tag } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+// get all products by user id
+router.get("/user/:id", async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      where: {
+        user_id: req.params.id,
+      },
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+        {
+          model: Tag,
+          attributes: ["tag_name"],
+        },
+      ],
+    });
+    const products = productData.map((product) => product.get({ plain: true }));
+    res.render("all-products", { products, logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// create a new product to sell
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newProduct = await Product.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newProduct);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// update a product by product id
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    Product.update(
+      {
+        // products: id, name, description, price, tag
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        tag: req.body.tag,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// ======⚠️Not discussed yet======== delete a product by id ⚠️
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this id!" });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
+// ☆•:*´¨`*:•.☆•:*´¨`*:•.Mengxue☆•:*´¨`*:•.☆•:*´¨`*:•.☆•:*´¨
