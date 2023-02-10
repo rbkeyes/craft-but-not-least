1e3e367dccc3c996dd9567e1debd45623a5a4755
mengxue xu <mxu4321@gmail.com>
2023-02-09 21:20:18 -0800

    updated file path in index under api folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 // ☆•:*´¨`*:•.☆•:*´¨`*:•.Mengxue☆•:*´¨`*:•.☆•:*´¨`*:•.☆•:*´¨
 const router = require('express').Router();
 // ⚠️ Import all of the API routes from /api/index.js
-const homeRoutes = require('./homeRoutes');
-const productRoutes = require('./productRoutes');
-const sellProductRoutes = require('./sellProductRoutes');
+const homeRoutes = require('../homeRoutes');
+const productRoutes = require('../productRoutes');
+const sellProductRoutes = require('../sellProductRoutes');
 
 router.use('/', homeRoutes);
 router.use('/products', productRoutes);
