4dd8090f716e234ba927e6ed429e46cc0ead5886
mengxue xu <mxu4321@gmail.com>
2023-02-10 17:06:20 -0800

    updated homeRoutes under ./controllers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,19 +1,25 @@
 // ☆•:*´¨`*:•.☆•:*´¨`*:•.Mengxue☆•:*´¨`*:•.☆•:*´¨`*:•.☆•:*´¨
-// ====== to be discussed =======    
 // for user profile page, includes user info, user's products, user's orders after login
-
 const router = require('express').Router();
+const { User, Product, Tag } = require('../models');
 
-
-
-
-
-
-
-
-
-
-
+// get user info by user id
+router.get('/:id', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id, {
+            include: [
+                {
+                    model: Product,
+                    attributes: ['id', 'name', 'description', 'price', 'tag'],
+                },
+            ],
+        });
+        const user = userData.get({ plain: true });
+        res.render('profile', { user, logged_in: req.session.logged_in });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 
