514b3ac79a069e4f5a6e8d7a57e46e5d591fda75
Reed Keyes <rbkeyes@gmail.com>
2023-02-12 12:07:19 -0800

    added get route for all products & added basic template to render list of product names in handlebars

--- public/js/listItemForm.js
+++ public/js/listItemForm.js
@@ -9,25 +9,27 @@ const listItemFormHandler = async (event) => {
   // not 100% sure what we'll need for for the photo upload, adding this for now but can edit later as needed 
   const photoUpload = document.getElementById('photo-upload');
   // there will be multiple tags, want to get all of them
-  const checkbox = document.querySelectorAll('#checkbox');
+  const checkbox = document.querySelectorAll('.checkbox');
   const tagName = document.querySelectorAll('#tag-name');
 
   // just realized I'll need to add code to register the checkboxes & get the associated tags.
 
-  if (itemName && description && listPrice && tag) { 
+  if (itemName && description && listPrice && checkbox) { 
     // add photoUpload when ready
     // Send the item data to the server
     const response = await fetch('/api/users/listItem', {
       method: 'POST',
-      body: JSON.stringify({ itemName, description, listPrice, tag }),
+      body: JSON.stringify({ itemName, description, listPrice, checkbox }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
       document.location.replace('/');
       // route to ?? page after POST
+      return;
     } else {
       alert('Unable to list item.');
+      return;
     }
   }
 };
