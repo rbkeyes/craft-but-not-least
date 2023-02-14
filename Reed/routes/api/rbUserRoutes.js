c7994ec203c299e540b1f8839aab5abdf824a97b
Reed Keyes <rbkeyes@gmail.com>
2023-02-13 22:00:13 -0800

    renamed rbhome to homepage.handlebars. moved unused rbroute to Reed folder

--- Reed/routes/api/rbUserRoutes.js
+++ Reed/routes/api/rbUserRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User } = require('../../../models');
 
 // CREATE new user
 router.post('/', async (req, res) => {
