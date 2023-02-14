c5f50213e8b875da6a1f83fb5435f73aa5046823
mengxue xu <mxu4321@gmail.com>
2023-02-13 19:22:23 -0800

    updated index in controllers & server.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,12 @@
 // ☆•:*´¨`*:•.☆•:*´¨`*:•.Mengxue☆•:*´¨`*:•.☆•:*´¨`*:•.☆•:*´¨
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 // const rbroutes = require('./rbroutes');
 
 // router.use('/', rbroutes);
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-
 
 router.use((req, res) => {
     res.status(404).end();
