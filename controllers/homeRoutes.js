6f491cd987201e287a41479a61d919350001d465
Reed Keyes <rbkeyes@gmail.com>
2023-02-13 22:24:31 -0800

    commented out some lines of code that seemed to prevent homepage from loading

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,15 +5,17 @@ const { User } = require("../models");
 
 router.get("/", async (req, res) => { // TODO: add withAuth
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ["password"] },
-      order: [["name", "ASC"]],
-    });
-    const users = userData.map((user) => user.get({ plain: true }));
+    // **rb** commented out some code to allow homepage to render **rb**
+    // const userData = await User.findAll({
+    //   attributes: { exclude: ["password"] },
+    //   order: [["name", "ASC"]],
+    // });
+    // const users = userData.map((user) => user.get({ plain: true }));
 
     res.render("homepage", {
-      users,
-      logged_in: req.session.logged_in,
+          // **rb** commented out some code to allow homepage to render **rb**
+      // users,
+      // logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
