8218b19e41fb7bc0f45842046fb1b8088496aa18
Reed Keyes <rbkeyes@gmail.com>
2023-02-13 22:52:13 -0800

    added route to new-listing form to home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -33,6 +33,30 @@ router.get("/login", (req, res) => {
   res.render("temp-login");
 });
 
+// **rb** adding new-listing route because I didn't see it anywhere. If it's here and I just missed it feel free to remove mine! **rb**
+// **rb** I haven't added withAuth to this route yet, will need it eventually **rb**
+router.get('/list-item', async (req, res) => {
+  try {
+      // get tag names from db
+      const tagsData = await Tag.findAll();
+
+      // 404 if nothing found
+      if (!tagsData) {
+          res.status(404).json('No tags found');
+      };
+
+      // if getting all: map array then get({plain: true}) before rendering
+      const tags = tagsData.map((tag) => tag.get({ plain: true }));
+      console.log(tags);
+      // render form with tags
+      res.render('list-item', {tags});
+      // logged_in: req.session.logged_in
+
+  } catch (err) {
+      res.status(500).json(err);
+  }
+});
+
 
 module.exports = router;
 // ☆•:*´¨`*:•.☆•:*´¨`*:•.Mengxue☆•:*´¨`*:•.☆•:*´¨`*:•.☆•:*´¨
