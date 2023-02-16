6b25c03ca29f0bf28b4892bc1889e4c9f49915db
Reed Keyes <rbkeyes@gmail.com>
2023-02-15 17:58:20 -0800

    added some code to Image model

---
+++ models/Image.js
@@ -0,0 +1,22 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Image extends Model { }
+
+// products: id, name, description, price, tag
+// 💥 **rb** Had to change the keys to be camel case so that the object created by handler would work in the POST. PLEASE KEEP AS CAMEL CASE **rb** 💥
+
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    file_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  }
+);
