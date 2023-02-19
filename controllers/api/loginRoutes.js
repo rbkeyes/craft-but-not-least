918bfd813dac5712cf6553c90896be9dc0de7542
mengxue xu <mxu4321@gmail.com>
2023-02-19 15:54:30 -0800

    edited profile-page.handlebars with syntax error check; updated profileRoutes to render profile page

--- controllers/api/loginRoutes.js
+++ controllers/api/loginRoutes.js
@@ -23,9 +23,10 @@ router.post('/login', async (req, res) => {
         req.session.save(() => {
             req.session.user_id = userData.id;
             req.session.logged_in = true;
-            
-            res.json({ user: userData, message: 'You are now logged in!' });
+
+            // res.json({ user: userData, message: 'You are now logged in!' });
             // console.log(req.session);
+            
         });
 
     } catch (err) {
