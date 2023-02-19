b2249946b81f562f18a0c3564f1b9eac46ab0f45
Reed Keyes <rbkeyes@gmail.com>
2023-02-18 22:54:04 -0800

    working on sell.js code

--- public/js/sell.js
+++ public/js/sell.js
@@ -10,20 +10,20 @@ const sellItemHandler = async (event) => {
 
   // **rb** fyi I changed to value of the checkboxes to the tag.id
   const checkbox = document.querySelectorAll('input[type="checkbox"]:checked');
-  let tags = []
+  let tagsId = []
   for (const checked of checkbox) {
-    tags.push(checked.value);
+    tagsId.push(checked.value);
   };
   // const product_tag = tags.toString();
-  console.log(product_tag);
+  console.log(tagsId);
 
 
 try {
-  if (product_name && product_description && product_price && product_tag) {
-    console.log({ product_name, product_description, product_price, product_tag })
+  if (product_name && product_description && product_price && tagsId) {
+    console.log({ product_name, product_description, product_price, tagsId })
     const response = await fetch('/api/sell', {
       method: 'POST',
-      body: JSON.stringify({ product_name, product_description, product_price, ...product_tag }),
+      body: JSON.stringify({ product_name, product_description, product_price, ...tagsId }),
       headers: { 'Content-Type': 'application/json' },
     });
 
