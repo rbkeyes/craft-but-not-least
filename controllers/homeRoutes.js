74b1e86e8701af15ad38564a0f20bd3dfb656359
Reed Keyes <114431225+rbkeyes@users.noreply.github.com>
2023-02-20 11:45:54 -0800

    Update homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => { // TODO: add withAuth
     res.render("homepage", {
       // **rb** commented out some code to allow homepage to render **rb**
       // users,
-      logged_in: loginStatus,
+      logged_in: req.session.logged_in    
     });
   } catch (err) {
     res.status(500).json(err);
