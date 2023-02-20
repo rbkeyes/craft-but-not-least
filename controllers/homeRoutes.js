060c568096ba2993a9af6ab4ba8d8dd78aaab3a0
mengxue xu <mxu4321@gmail.com>
2023-02-19 20:20:15 -0800

    to render profile-page & sell page, edited login&progifle-page handlebars, homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,7 +17,6 @@ router.get("/", async (req, res) => { // TODO: add withAuth
     res.render("homepage", {
       // **rb** commented out some code to allow homepage to render **rb**
       // users,
-      // MX: commented out below 021923
       // logged_in: req.session.logged_in,
     });
   } catch (err) {
@@ -25,7 +24,7 @@ router.get("/", async (req, res) => { // TODO: add withAuth
   }
 });
 
-// ❄️MX: added below for testing only, to render profile page
+// ❄️MX: added below for testing only
 // router.get("/profile", async (req, res) => { 
 //   try {
 
@@ -37,9 +36,9 @@ router.get("/", async (req, res) => { // TODO: add withAuth
 
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
-    res.redirect("/");
-    // MX: added below 021923
-    // res.redirect("/profile")
+    // ❄️MX: temporarily added line 42 to render profile page
+    // res.redirect("/");
+    res.redirect("/profile")
     return;
   }
   res.render("login")
@@ -53,12 +52,31 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
+// ❄️MX: temporarily commented out, see router.get('/sell') below line 80
+// router.get('/sell', withAuth, async (req, res) => {
+//   try {
+//     // get tag names from db
+//     const tagsData = await Tag.findAll({ attributes: ['id','tag_name'] });
 
-router.get('/sell', withAuth, async (req, res) => {
-  try {
-    // get tag names from db
-    const tagsData = await Tag.findAll({ attributes: ['id','tag_name'] });
+//     // 404 if nothing found
+//     if (!tagsData) {
+//       res.status(404).json('No tags found');
+//     };
+//     // if getting all: map array then get({plain: true}) before rendering
+//     const tags = tagsData.map((tag) => tag.get({ plain: true }));
+//     console.log(tags);
+//     // render form with tags
+//     res.render('sell', { 
+//       image_path: null,
+//       tags, 
+//       logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
+<<<<<<< HEAD
     // 404 if nothing found
     if (!tagsData) {
       res.status(404).json('No tags found');
@@ -78,6 +96,9 @@ router.get('/sell', withAuth, async (req, res) => {
 });
 
 // ❄️MX: temp route for rendering sell page
+=======
+// ❄️MX: temp route for rendering sell page (sell page will require login)
+>>>>>>> b96f0c4 (to render profile-page & sell page, edited login&progifle-page handlebars, homeRoutes.js)
 router.get('/sell', async (req, res) => {
   try {
     res.render('sell', {
