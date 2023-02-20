c8f3f351b1abfbef71eb58c00c918aaa771c90e4
mengxue xu <mxu4321@gmail.com>
2023-02-19 20:03:00 -0800

    added temp routes in homeRoutes

--- controllers/api/sellRoutes.js
+++ controllers/api/sellRoutes.js
@@ -23,17 +23,17 @@ const upload = multer({ storage: storage }).single('image_file');
 
 // ⤵️============ ✅tested with json object input: 200 ok =================
 // create a new product to sell ((localhost:3001/api/sell)
-// router.post("/", withAuth, async (req, res) => {
+// router.post("/", async (req, res) => {
 //   try {
 //     const newProduct = await Product.create({
 //       ...req.body,
-//       user_id: req.session.user_id,
+//       // user_id: req.session.user_id,
 //     });
 //     // comment out below after login is working
-//     res.status(200).json({
-//       message: "Product has been successfully added!",
-//       product: req.body,
-//     });
+//     // res.status(200).json({
+//     //   message: "Product has been successfully added!",
+//     //   product: req.body,
+//     // });
 //     res.render('sell');
 //     // ⭐️TODO: add login session once login is working⤵️
 //     // res.render('new-Product', { products, logged_in: req.session.logged_in });
@@ -63,6 +63,7 @@ router.post('/', withAuth, async (req, res) => {
       });
       // bulk create ProductTag using array of objects created above
       const newProductTags = await ProductTag.bulkCreate(tagIdArr);
+
       res.status(200).json(newProductTags);    
   } catch (err) {
     console.log(err);
