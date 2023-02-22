8fa930e574435fce121adab66163208c160ff768
mengxue xu <mxu4321@gmail.com>
2023-02-22 11:02:39 -0800

    updated homeroutes/products handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -54,28 +54,28 @@ router.get("/signup", (req, res) => {
 });
 
 // ❄️MX: temporarily commented out, see router.get('/sell') in the next section
-// router.get('/sell', withAuth, async (req, res) => {
-//   try {
-//     // get tag names from db
-//     const tagsData = await Tag.findAll({ attributes: ['id','tag_name'] });
+router.get('/sell', withAuth, async (req, res) => {
+  try {
+    // get tag names from db
+    const tagsData = await Tag.findAll({ attributes: ['id','tag_name'] });
 
-//     // 404 if nothing found
-//     if (!tagsData) {
-//       res.status(404).json('No tags found');
-//     };
-//     // if getting all: map array then get({plain: true}) before rendering
-//     const tags = tagsData.map((tag) => tag.get({ plain: true }));
-//     console.log(tags);
-//     // render form with tags
-//     res.render('sell', { 
-//       image_path: null,
-//       tags, 
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    // 404 if nothing found
+    if (!tagsData) {
+      res.status(404).json('No tags found');
+    };
+    // if getting all: map array then get({plain: true}) before rendering
+    const tags = tagsData.map((tag) => tag.get({ plain: true }));
+    console.log(tags);
+    // render form with tags
+    res.render('sell', { 
+      image_path: null,
+      tags, 
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // ❄️MX: temp route for rendering sell page (sell page will require login)
 // router.get('/sell', async (req, res) => {
